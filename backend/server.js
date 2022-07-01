30f79dc7f8263f596128cf372dc32b52128cbe07
Joseph Mania <techmaniacc@localhost.localdomain>
2022-07-01 12:33:05 +0300

    Working wth express, routes

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
 require('dotenv').config()
 const express = require('express')
+const workoutRoutes=require("./routes/workout")
 const { reset } = require('nodemon')
 const app = express()
 
 //middleware
+app.use(express.json())
 app.use((req, res, next)=>{
     console.log(req.path, req.method)
     next()
 })
 
 //routes
-app.get('/',(req, res )=>{
-    res.json({mssg:"Welcome home buddy"})
-})
+app.use('/api/workouts', workoutRoutes)
 
 app.listen(process.env.PORT, ()=>{
     console.log("Listenin on port 4000!!mania")
